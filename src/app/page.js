03e9f5c4eb8a7f1e32368f909d342174b6bf2a5b
YeswanthRajakumartw <yeswanth.rajakumar@thoughtworks.com>
2024-01-27 14:06:23 +0530

    add: footer

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,15 @@ import { Hero } from "./Hero";
 import { SideProjects } from "./SideProjects";
 import { Talks } from "./Talks";
 import { Contact } from "./Contact";
+
+const Footer = () => {
+  return (
+    <div className="mt-5 text-sm text-gray-700 text-center pb-1">
+      Built with ♥️ by Yeswanth Rajakumar @ {new Date().getFullYear()}
+    </div>
+  );
+};
+
 export default function Home() {
   return (
     <>
@@ -12,7 +21,7 @@ export default function Home() {
       <SideProjects />
       <Talks />
       <Contact />
-      {/* <Footer /> */}
+      <Footer />
     </>
   );
 }
